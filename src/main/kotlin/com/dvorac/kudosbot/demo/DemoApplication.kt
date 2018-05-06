5fa61efde3b7050758d627caec1569647a5be020
dvorac <dvorac@solstice-mobile.com>
2018-05-06 12:39:37 -0500

    project init

---
+++ src/main/kotlin/com/dvorac/kudosbot/demo/DemoApplication.kt
@@ -0,0 +1,11 @@
+package com.dvorac.kudosbot.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
